91a56a58d7529c6ef507b9591d47a8ddda67323c
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-12-02 18:00:41 +0900

    feat: 프롬프트 생성 기능 추가

---
+++ src/aggregates/counselPrompts/applications/commands/CreatePrompt/CreatePrompt.command.ts
@@ -0,0 +1,29 @@
+import { HttpStatus } from "@nestjs/common";
+import { CounselPromptType } from "~/src/shared/enums/CounselPromptType.enum";
+import { HttpStatusBasedRpcException } from "~/src/shared/filters/exceptions";
+
+export class CreatePromptCommand {
+  constructor(public readonly props: CreatePromptCommandProps) {
+    this.validate(props);
+  }
+
+  private validate(props: CreatePromptCommandProps): void {
+    if (props.promptType === null || props.promptType === undefined) {
+      throw new HttpStatusBasedRpcException(HttpStatus.BAD_REQUEST, "프롬프트 타입은 필수입니다.");
+    }
+    if (!Object.values(CounselPromptType).includes(props.promptType)) {
+      throw new HttpStatusBasedRpcException(HttpStatus.BAD_REQUEST, "유효하지 않은 프롬프트 타입입니다.");
+    }
+  }
+}
+
+interface CreatePromptCommandProps {
+  persona?: string;
+  context?: string;
+  instruction?: string;
+  tone?: string;
+  additionalPrompt?: string;
+  promptType: CounselPromptType;
+  description?: string;
+  version?: string;
+}
