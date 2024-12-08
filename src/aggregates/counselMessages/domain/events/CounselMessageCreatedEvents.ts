836c7f5eb2344bbb83c1e53ee9c9d3ab454ff9b1
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-12-08 17:50:00 +0900

    feat: 상담메시지생성이벤트 발행기능 추가

---
+++ src/aggregates/counselMessages/domain/events/CounselMessageCreatedEvents.ts
@@ -0,0 +1,15 @@
+import { toBinary } from "@bufbuild/protobuf";
+import { CounselMessageCreatedPayload, CounselMessageCreatedPayloadSchema } from "~/src/gen/v1/message/counsel_pb";
+import { DomainEvent } from "~/src/shared/core/domain/events/DomainEvent";
+
+export class CounselMessageCreatedEvent extends DomainEvent {
+  static readonly topic = "counsel.message.created";
+  public readonly payload: CounselMessageCreatedPayload;
+  public readonly binary: Uint8Array;
+
+  constructor(payload: CounselMessageCreatedPayload) {
+    super();
+    this.payload = payload;
+    this.binary = toBinary(CounselMessageCreatedPayloadSchema, payload, { writeUnknownFields: false });
+  }
+}
--- src/aggregates/counselMessages/infrastructures/adaptors/psql.counselMessages.repository.adaptor.ts
+++ src/aggregates/counselMessages/infrastructures/adaptors/psql.counselMessages.repository.adaptor.ts
@@ -7,13 +7,30 @@ import {
 import { CounselMessagesEntity } from "~/src/shared/core/infrastructure/entities/CounselMessages.entity";
 import { CounselMessages } from "~/src/aggregates/counselMessages/domain/CounselMessages";
 import { PsqlCounselMessagesMapper } from "./mapper/psql.counselMessages.mapper";
+import { Inject } from "@nestjs/common";
+import { KAFKA_CLIENT } from "~/src/shared/core/infrastructure/Config";
+import { ClientKafka } from "@nestjs/microservices";
 
 export class PsqlCounselMessagesRepositoryAdaptor implements CounselMessagesRepositoryPort {
-  constructor(@InjectRepository(CounselMessagesEntity) private readonly counselMessagesRepository: Repository<CounselMessagesEntity>) {}
+  constructor(
+    @InjectRepository(CounselMessagesEntity) private readonly counselMessagesRepository: Repository<CounselMessagesEntity>,
+    @Inject(KAFKA_CLIENT) private readonly kafkaProducer: ClientKafka,
+  ) {}
+
+  async publishDomainEvents(counselMessage: CounselMessages): Promise<void> {
+    const domainEvents = counselMessage.domainEvents;
+    for (const domainEvent of domainEvents) {
+      this.kafkaProducer.emit(domainEvent.topic, domainEvent.binary);
+    }
+    counselMessage.clearEvents();
+  }
 
   async create(counselMessage: CounselMessages): Promise<CounselMessages> {
     const counselMessagesEntity = PsqlCounselMessagesMapper.toEntity(counselMessage);
     const createdCounselsEntity = await this.counselMessagesRepository.save(counselMessagesEntity);
+
+    await this.publishDomainEvents(counselMessage);
+
     return PsqlCounselMessagesMapper.toDomain(createdCounselsEntity);
   }
 
@@ -32,6 +49,11 @@ export class PsqlCounselMessagesRepositoryAdaptor implements CounselMessagesRepo
     };
 
     const counselMessagesEntities = await this.counselMessagesRepository.find(findManyOptions);
-    return counselMessagesEntities.map((counselMessagesEntity) => PsqlCounselMessagesMapper.toDomain(counselMessagesEntity));
+    const counselMessageList = counselMessagesEntities.map((counselMessagesEntity) => PsqlCounselMessagesMapper.toDomain(counselMessagesEntity));
+    if (counselMessageList.length === 0) {
+      counselMessageList.forEach((counselMessage) => this.publishDomainEvents(counselMessage));
+    }
+
+    return counselMessageList;
   }
 }
